5e492baf037749905060d92c73fb590456ab3954
Agustin Bravo Rubio <abravo@itx.com>
2020-01-27 20:04:08 -0300

    created proyect

---
+++ hoysesale/src/main/java/com/dh/hoysesale/HoysesaleApplication.java
@@ -0,0 +1,13 @@
+package com.dh.hoysesale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoysesaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoysesaleApplication.class, args);
+	}
+
+}
---
+++ hoysesale/src/test/java/com/dh/hoysesale/HoysesaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.hoysesale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoysesaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
