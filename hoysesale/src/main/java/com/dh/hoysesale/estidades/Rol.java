2994a050a720a7f28e28ea6a825fd2aafb12b5a4
Matias Aguilera <aguileramati50@gmail.com>
2020-01-27 21:04:47 -0300

    creacion entidades: Bebidas, Categorias, Usuario, Rol

---
+++ hoysesale/src/main/java/com/dh/hoysesale/estidades/Rol.java
@@ -0,0 +1,9 @@
+package com.dh.hoysesale.estidades;
+
+public class Rol{
+	
+	private int id_rol;
+	private String descripcion;
+	private boolean vigencia;
+	
+}
---
+++ hoysesale/src/main/java/com/dh/hoysesale/estidades/Usuario.java
@@ -0,0 +1,22 @@
+package com.dh.hoysesale.estidades;
+
+import java.util.Date;
+
+public class Usuario{
+	
+	private int id_usuario;
+	private String nombre;
+	private String apellido;
+	private Date fecha_de_nacimiento;
+	private String domicilio;
+	private String ciudad;
+	private String provincia;
+	private String pais;
+	private int telefono;
+	private String email;
+	private String imagen;
+	private Rol rol;
+	
+	
+	
+}
