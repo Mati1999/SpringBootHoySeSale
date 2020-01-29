dd4f8790bd9e6dac378a00edf191f592862c940e
Agustin Bravo Rubio <abravo@itx.com>
2020-01-29 18:43:54 -0300

    added Services

--- hoysesale/src/main/java/com/dh/hoysesale/controllers/HomeController.java
+++ hoysesale/src/main/java/com/dh/hoysesale/controllers/HomeController.java
@@ -1,34 +1,27 @@
 package com.dh.hoysesale.controllers;
 
-import java.util.Locale;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import com.dh.hoysesale.IServices.IServiceProducts;
 
 
 @Controller
 public class HomeController {
 
-	/*GetMapping:
-		-- Completar la lista de productos.
-		-- Completar la lista de filtros.
-	  PostMapping:
-		-- Para Editar / Eliminar
-	  PutMapping:
-		-- Para agregar*/
-		
-	
+	@Autowired
+	@Qualifier("ServiceProductsImpl")
+	private IServiceProducts serviceProducts;
 	
 
-	@RequestMapping(value="/products", method= RequestMethod.GET)
-	public String productController (Locale locale, Model model) {
+	@GetMapping(value="/products")
+	public String productController (Model model) {
+		
+		
+		
 		return "products";
 	}
-	
-	
-	
 	
 }
